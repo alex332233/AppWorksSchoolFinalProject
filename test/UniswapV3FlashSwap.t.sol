ff46f7115bf0ccd3fee7a4acdcd582697687b93e
Alex Su <alex332233@gmail.com>
2024-01-10 23:22:59 +0800

    complete UniswapV3FlashSwaptoV2Swap

---
+++ test/UniswapV3FlashSwap.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "../src/UniswapV3FlashSwap.sol";
+
+contract UniswapV3FlashSwapTest is Test {
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address private constant TURBO = 0xA35923162C49cF95e6BF26623385eb431ad920D3;
+    address private constant univ3pool =
+        0x7baecE5d47f1BC5E1953FBE0E9931D54DAB6D810;
+    address private constant univ2pool =
+        0x455d4d19aCA31C7530D75a32f98cf28d98365587;
+
+    IWETH private weth = IWETH(WETH);
+    IERC20 private turbo = IERC20(TURBO);
+
+    UniswapV3FlashSwap private uniFlashSwapContract = new UniswapV3FlashSwap();
+
+    function setUp() public {
+        vm.createSelectFork(vm.envString("RPC_URL"), 17465000);
+    }
+
+    function testFlashSwap() public {
+        // TURBO / WETH pool
+        // address pool0 = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
+        // uint24 fee0 = 3000;
+        // address pool1 = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+        uint24 fee1 = 3000;
+        uint256 amountIn = 19817473602235238009479; // 19,817.473602235238009479 Turbo
+
+        // Approve WETH fee
+        uint wethMaxFee = 1e18;
+        weth.deposit{value: wethMaxFee}();
+        weth.approve(address(uniFlashSwapContract), wethMaxFee);
+
+        uint balBefore = weth.balanceOf(address(this));
+        uniFlashSwapContract.flashSwap(univ3pool, fee1, WETH, TURBO, amountIn);
+        uint balAfter = weth.balanceOf(address(this));
+
+        if (balAfter >= balBefore) {
+            console.log("WETH profit", balAfter - balBefore);
+        } else {
+            console.log("WETH loss", balBefore - balAfter);
+        }
+    }
+}
